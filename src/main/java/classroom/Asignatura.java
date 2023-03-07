bde9ee9cf962420196920ae9aa1418654b0f2db2
MenGonz <semendozag@unal.edu.co>
2023-03-06 19:14:33 -0500

    terminado 1

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -8,23 +8,15 @@ public class Asignatura {
     public int codigoExterno;
     public Tipo tipo;
 
-    public Asignatura() {
-        this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
-    }
+    // Sobrecarga de métodos Asignatura()
+    public Asignatura() { this("Sin nombre",Tipo.FUNDAMENTACION); }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
-    }
+    public Asignatura(int codigoExterno,Tipo tipo) { this("Sin nombre", 0, codigoExterno,tipo); }
 
-    public Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
-    }
-
-    public Asignatura(String nombre, Tipo tipo) {
-        this(nombre, 0, 0,tipo);
-    }
-
+    public Asignatura(Tipo tipo, int codigoInterno) { this("Sin nombre", codigoInterno, 0,tipo); }
+    
+    public Asignatura(String nombre, Tipo tipo) { this(nombre, 0, 0,tipo); }
+    
     public Asignatura(String nombre, int codigoInterno, int codigoExterno,Tipo tipo) {
         this.nombre = nombre;
         this.codigoInterno = codigoInterno;
@@ -32,30 +24,20 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
+
+    //sobrecarga de métodos cambiarDatos()
     public void cambiarDatos(int codigoInterno, int codigoExterno, String nombre,Tipo tipo) {
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
         this.tipo = tipo;
     }
+    public void cambiarDatos(int codigoExterno) { this.codigoExterno = codigoExterno; }
 
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
+    public void cambiarDatos(String nombre) { this.nombre = nombre; }
 
-    public void cambiarDatos(int codigoExterno) {
-        this.codigoExterno = codigoExterno;
-    }
-
-    public void cambiarDatos(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setCodigoInterno(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
+    //setters 
+    public void setCodigoInterno(int codigoInterno) { this.codigoInterno = codigoInterno; }
     
-    public void setCodigoInterno(double codigoInterno) {
-        this.codigoInterno = (int) codigoInterno;
-    }
+    public void setCodigoInterno(double codigoInterno) { this.codigoInterno = (int) codigoInterno; }
 }
--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -5,9 +5,10 @@ public class Grupo {
     public Persona[] estudiantes;
     public Persona profesor;
     public Asignatura asignatura;
-    public final int codigo = 0;
+    public int codigo = 0;
     public String horario;
 
+    //sobrecarga de métodos Grupo()
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
@@ -15,18 +16,18 @@ public class Grupo {
         this.codigo = codigo;
         this.horario = horario;
     }
-
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        Persona[] personas = new Persona[cantidadEstudiantes];
-        this(personas, profesor, asignatura, codigo, horario);
+        this(new Persona[cantidadEstudiantes], profesor, asignatura, codigo, horario);
+        
     }
-
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
     }
 
+
+    //sobrecarga de métodos cambiarEstudiante()
     void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
         for (int i = 0; i < estudiantes.length; i++) {
             if (estudiantes[i].getCedula() == estudianteViejo.getCedula()) {
@@ -35,7 +36,6 @@ public class Grupo {
             }
         }
     }
-    
     void cambiarEstudiante(int indice, Persona estudiante) {
         estudiantes[indice] = estudiante;
     }
